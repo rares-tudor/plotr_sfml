0f6f42057e3d96b01d2743e47fb46686820d1817
rares-tudor <rarestudor04@gmail.com>
2020-10-14 12:48:54 +0200

    updated coordinate system

--- plotr/main.cpp
+++ plotr/main.cpp
@@ -9,6 +9,10 @@ int main()
 	window.clear(sf::Color(237,237,237));
 	int x_size = window.getSize().x, y_size = window.getSize().y; // Defining size of the window, customizable
 
+	// Creating the circle to indicate which point we are at
+	sf::CircleShape pt(3.f);
+	pt.setFillColor(sf::Color::Red);
+
 	// Creating the pixel which will create the grid, drawing it iteratively on the canvas
 	sf::RectangleShape pixel(sf::Vector2f(2.f, 2.f));
 	sf::CircleShape tri(8.f);
@@ -48,12 +52,19 @@ int main()
 			else if (event.type == sf::Event::Resized)
 				window.setSize(sf::Vector2u(event.size.width, event.size.height));
 		}
-		window.clear(sf::Color(237,237,237));
+
+		window.clear(sf::Color(237,237,237)); // Clearing window with grey color
+
 		// Drawing coordinate system
 		for (int x = 0; x < window.getSize().x; x += 2)
 		{
-			pixel.setPosition(sf::Vector2f(x, y_size / 2));
+			pixel.setPosition(sf::Vector2f(x, (y_size-60) / 2));
 			window.draw(pixel);
+			if (x % 160 == 0)
+			{
+				pt.setPosition(pixel.getPosition());
+				window.draw(pt);
+			}
 			if (x == (window.getSize().x - 2))
 			{
 				tri.setRotation(90);
@@ -62,10 +73,15 @@ int main()
 			}
 		}
 
-		for (int y = 0; y < window.getSize().y; y += 2)
+		for (int y = 0; y <= window.getSize().y-60; y += 2)
 		{
 			pixel.setPosition(sf::Vector2f(x_size / 2, y));
 			window.draw(pixel);
+			if (y % 160 == 0)
+			{
+				pt.setPosition(pixel.getPosition());
+				window.draw(pt);
+			}
 			if (y == 0)
 			{
 				tri.setRotation(0);
